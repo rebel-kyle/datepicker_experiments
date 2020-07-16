386d91d3d5ccbacea86f797f41e3c6cd7603006f
Kyle Reilly <kyle@rebelspacetech.com>
2020-07-16 14:04:15 -0700

    first commit datepickers are updating state utilizing hooks here

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+import moment from 'moment'
 
 function App() {
+  const [ startDate, setStartDate ] = useState(null)
+  const [ endDate, setEndDate ] = useState(null)
+
+  const handleStartChange = (date) => {
+    setStartDate(date)
+  }
+
+  const handleEndChange = (date) => {
+    setEndDate(date)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="results">
+          <p>Start Time: {startDate===null ? "Enter Date" : startDate.toString()}</p>
+        </div>
+        <div className="timecontainerouter">
+          <div className="starttimecontainer">
+            <DatePicker
+              name="starttime"
+              placeholderText="Enter start date"
+              dateFormat="yyyy-MM-dd HH:mm"
+              showTime
+              isClearable
+              selected={startDate}
+              onChange={handleStartChange}
+              maxDate={endDate}
+              selectsStart
+              timeInputLabel="Time:"
+              showTimeInput
+            />
+          </div>
+          <div className="endtimecontainer">
+            <DatePicker
+              name="endtime"
+              placeholderText="Enter end date"
+              dateFormat="yyyy-MM-dd HH:mm"
+              showTime
+              isClearable
+              selected={endDate}
+              onChange={handleEndChange}
+              startDate={startDate}
+              selectsEnd
+              timeInputLabel="Time:"
+              showTimeInput
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
